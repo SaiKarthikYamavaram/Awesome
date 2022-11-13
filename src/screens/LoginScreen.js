7a6191cca762757b7d17d66c9eeb5b01e6a42d27
SaiKarthikYamavaram <ysaikarthiksep01@gmail.com>
2022-11-13 13:54:22 +0530

    initial commit
    initial commit
    
    initial commit

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,122 @@
+import {Button, Icon, Input, Layout, Text} from '@ui-kitten/components';
+import React from 'react';
+import {Image, StyleSheet, useWindowDimensions} from 'react-native';
+
+
+const LoginScreen = () => {
+    const {height, width} = useWindowDimensions();
+
+
+    const [passwordVisibility, setPasswordVisibility] = React.useState(true);
+    const [companyName, setCompanyName] = React.useState('');
+    const [userName, setUserName] = React.useState('');
+    const [password, setPassword] = React.useState('');
+
+    const onSubmit = () => {
+    };
+
+
+    return (
+        <Layout style={[styles.containerStyle, {flexDirection: width > height ? 'row' : 'column'}]} level={'1'}>
+            <Layout style={[styles.containerStyle, {width: '100%'}]}>
+                <Image style={{flex: 1, width: Math.min(width, height)}} resizeMethod={'resize'} resizeMode="contain"
+                       source={require('../assets/images/Invoice.png')}/>
+            </Layout>
+            <Layout style={{flex: 1, alignItems: 'flex-start', width: '100%'}}>
+                <Text status='primary' style={styles.header} size="large">Login</Text>
+                <Input
+                    placeholder='Company name'
+                    value={companyName}
+                    size="large"
+                    accessoryLeft={<Icon name='home-outline'/>}
+                    style={styles.inputStyle}
+                    onChangeText={setCompanyName}
+                />
+
+                <Input
+                    placeholder='Username'
+                    value={userName}
+                    size="large"
+                    textContentType={'username'}
+                    accessoryLeft={<Icon name='person-outline'/>}
+                    style={styles.inputStyle}
+                    onChangeText={setUserName}
+                />
+                <Input
+                    placeholder='Password'
+                    value={password}
+                    size="large"
+                    secureTextEntry={passwordVisibility}
+                    textContentType={'password'}
+                    accessoryLeft={<Icon name='lock-outline'/>}
+                    accessoryRight={<Icon onPress={() => setPasswordVisibility(!passwordVisibility)}
+                                          name={passwordVisibility ? 'eye-outline' : 'eye-off-outline'}/>}
+                    style={styles.inputStyle}
+                    onChangeText={setPassword}
+                />
+
+                <Button size="large" onPress={onSubmit} style={styles.button}>
+                    {() => <Text style={[styles.labelStyle, styles.btnLabel]}>Login</Text>}
+                </Button>
+            </Layout>
+        </Layout>
+    );
+};
+
+const styles = StyleSheet.create({
+    containerStyle: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        padding: '4%',
+        flex: 1,
+    },
+    inputStyle: {
+        width: '100%',
+        color: 'black',
+        marginTop: 16,
+    },
+    button: {
+        width: '100%',
+        borderRadius: 8,
+        marginVertical: 8,
+    },
+    btnLabel: {
+        fontSize: 18,
+        color: 'white',
+    },
+    counterButtonGroup: {
+        'margin': 2,
+    },
+    actionButtonGroup: {
+        flexDirection: 'column',
+        width: '100%',
+    },
+    header: {
+        width: '100%',
+        fontWeight: 'bold',
+        fontSize: 34,
+        fontFamily: 'Roboto-Black',
+    },
+    counterContainer: {
+        width: '100%',
+        paddingHorizontal: 4,
+        marginVertical: 12,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        backgroundColor: 'transparent',
+    },
+    selectorStyle: {
+        marginTop: 12,
+    },
+    labelStyle: {
+        fontSize: 13.5,
+        marginBottom: 4,
+    },
+
+
+});
+
+
+export default LoginScreen;
